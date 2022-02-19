07abcb41bc515787de7c549b6c5f353201d02626
legan- <marasanak@gmail.com>
2022-02-19 22:38:48 +0000

    feat: ts + prettier + eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 
-const App = () => {
-  const [images, setImages] = useState();
+import './App.css';
+import { Image } from './types';
+
+export const App = () => {
+  const [images, setImages] = useState<Image[]>();
 
   useEffect(() => {
     fetch('images?limit=10')
@@ -17,17 +19,14 @@ const App = () => {
   }, []);
 
   return (
-    <div className='app'>
-      {
-        images && images.map(img => (
-          <div key={img.id} >
-            <img src={`${img.url}.jpg`} alt=''/>
-            <img src={`${img.user.profile_image}.webp`} alt=''/>
+    <div className="app">
+      {images &&
+        images.map(img => (
+          <div key={img.id}>
+            <img src={`${img.url}.jpg`} alt="" />
+            <img src={`${img.user.profile_image}.webp`} alt="" />
           </div>
-        ))
-      }
+        ))}
     </div>
   );
-}
-
-export default App;
+};
