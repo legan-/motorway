6683ec8c3ba86c517eb5aeb228fb970d71e59b63
legan- <marasanak@gmail.com>
2022-02-20 13:00:55 +0000

    feat: add some styles to the card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,9 +8,27 @@ type CardProps = {
   details: Image;
 };
 
-export const Card: VFC<CardProps> = ({ details }) => {
-  return <div className="card">{details.id}</div>;
+export const Card: VFC<CardProps> = ({
+  details: {
+    url: imageUrl,
+    alt_description: imageAlt,
+    user: { profile_image: profileImageUrl, name, first_name: firstName, last_name: lastName },
+  },
+}) => {
+  return (
+    <div className="card">
+      <img className="image" src={`${imageUrl}.jpg`} alt={imageAlt} />
+      <div className="user-details">
+        {profileImageUrl.length ? (
+          <img className="profile-image" src={`${profileImageUrl}.webp`} alt={name} />
+        ) : (
+          // profile picture placeholder
+          <div className="profile-image profile-image-placeholder">
+            {`${firstName.charAt(0)}${lastName.charAt(0)}`}
+          </div>
+        )}
+        <div className="username">{name}</div>
+      </div>
+    </div>
+  );
 };
-
-//         <img src={`${img.url}.jpg`} alt="" />
-//         <img src={`${img.user.profile_image}.webp`} alt="" />
